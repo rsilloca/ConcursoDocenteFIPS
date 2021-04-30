f8388d1a480191cbdc2ceb3119d673c492f050d8
Hristo Roque <aroquega@unsa.edu.pe>
2021-04-29 23:46:41 -0500

    añade lombok y anotaciones a clase Contest

--- src/main/java/edu/unsa/concursodocente/models/Contest.java
+++ src/main/java/edu/unsa/concursodocente/models/Contest.java
@@ -4,14 +4,28 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class Contest {
     @Id
     private long id;
+
     private int academicYear;
+
+    @Temporal(TemporalType.DATE)
     private Date callDate;
+
+    @Temporal(TemporalType.DATE)
     private Date ballotDrawDate;
+
+    @Temporal(TemporalType.DATE)
     private Date masterClassDate;
+
+    @Temporal(TemporalType.DATE)
     private Date facultyCouncilDate;
 }
